7342f361cd7d32b1b39fa8a4561dfbe0f8fcf03b
Motasim <MotaSay@gmail.com>
2015-05-06 20:31:23 +0100

    Added plot 2

---
+++ plot2.R
@@ -0,0 +1,13 @@
+cons <- read.csv(unz('../exdata-data-household_power_consumption.zip', 'household_power_consumption.txt'),
+                 sep=';', na.strings='?')
+
+# keep the data between 01/02/2007 and 02/02/2007
+relevant.rows <- cons$Date == '1/2/2007' | cons$Date == '2/2/2007'
+data <- cons[relevant.rows,]
+
+dates <- paste(data$Date, data$Time, sep='|')
+dates <- strptime(dates, format='%d/%m/%Y|%H:%M:%S', tz='GMT')
+
+png(filename='plot2.png', width=480, height=480)
+plot(x=dates, y=data$Global_active_power, type="l", ylab="Global Active Power (kilowats)")
+dev.off()
